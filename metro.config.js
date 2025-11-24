be1af23ef5a9e7a4c4a18dd5936fee72cd406da4
MicheleJohn <michele.troisi@itsvil.it>
2025-11-24 14:55:40 +0100

    feat: updated nativewind to preview v5

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const { getSentryExpoConfig } = require('@sentry/react-native/metro');
-const { withNativeWind } = require('nativewind/metro');
+const { withNativewind } = require('nativewind/metro');
 
 const baseConfig = getDefaultConfig(__dirname);
 const sentryConfig = getSentryExpoConfig(__dirname);
@@ -14,8 +14,6 @@ sentryConfig.resolver.blockList = [/context\.md$/, /context\.txt$/];
 
 const mergedConfig = mergeConfig(baseConfig, sentryConfig);
 
-const finalConfig = withNativeWind(mergedConfig, {
-  input: './global.css',
-});
+const finalConfig = withNativewind(mergedConfig);
 
 module.exports = finalConfig;
