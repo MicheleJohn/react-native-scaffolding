0958064c5e46e7ffe9b72611c755e083fe5cb478
MicheleJohn <michele.troisi@itsvil.it>
2025-11-25 15:39:14 +0100

    fix: updated node types

---
+++ env.d.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+/// <reference types="node" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly NODE_ENV: 'development' | 'production' | 'test';
+    readonly EXPO_PUBLIC_API_URL: string;
+    readonly EXPO_PUBLIC_ENV: 'development' | 'production' | 'staging';
+    readonly EXPO_PUBLIC_SENTRY_DSN?: string;
+  }
+}
