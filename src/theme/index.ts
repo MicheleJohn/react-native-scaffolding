a437a3c1a7e07576e86ea12c00bf0da544c01974
MicheleJohn <mikit.95@hotmail.it>
2025-12-01 10:38:33 +0100

    feat: add design system colors and theme configuration

---
+++ src/theme/index.ts
@@ -0,0 +1,4 @@
+export { colors, getColor, type ColorPalette } from './colors';
+export { spacing } from './spacing';
+export { typography } from './typography';
+export { shadows } from './shadows';
