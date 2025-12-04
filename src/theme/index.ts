0186e59714e94a1315b87bd73dea902fc18712eb
MicheleJohn <michele.troisi@itsvil.it>
2025-12-05 00:40:44 +0100

    fix: deleted import

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,4 +1,3 @@
-export { type ColorPalette, colors, getColor } from './colors';
 export { shadows } from './shadows';
 export { spacing } from './spacing';
 export { typography } from './typography';
